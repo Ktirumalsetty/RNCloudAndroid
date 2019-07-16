9085c6feb0f5fe55be28cdd95e6ff88685d6738c
KondalRao <ktirumalsetty@phsi.us>
2019-07-16 19:13:20 +0530

    My Preferences UI design

--- app/src/main/java/com/rncloud/android/view/activity/MyPreferencesActivity.kt
+++ app/src/main/java/com/rncloud/android/view/activity/MyPreferencesActivity.kt
@@ -14,10 +14,10 @@ class MyPreferencesActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_preferences)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
 
 }
